2d62b8c1ef10fa83dcbc3bea7e0ea4bdc9e894c4
PhilGuan <gsf1395986293>
2014-05-19 18:07:03 +0800

    1.0.2

---
+++ MySecondASPProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySecondASPProject.Startup))]
+namespace MySecondASPProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
